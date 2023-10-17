95f6ddf232c817354fc308ee334b9a11b7561541
vaidehi <vaidehi@karmalife.ai>
2023-10-17 12:13:16 +0530

    Removed the key

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-const form = document.getElementById('chat-form');
-const mytextInput = document.getElementById('mytext');
-const responseTextarea = document.getElementById('response');
+const form = document.getElementById("chat-form");
+const mytextInput = document.getElementById("mytext");
+const responseTextarea = document.getElementById("response");
 
-const API_KEY = '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';
+const API_KEY = "";
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const mytext = mytextInput.value.trim();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const mytext = mytextInput.value.trim();
 
-    if (mytext) {
-        try {
-            const response = await fetch('https://api.openai.com/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${API_KEY}`,
-                },
-                body: JSON.stringify({
-                    "model": "gpt-3.5-turbo",
-                    "messages": [
-                    {
-                    "role": "system",
-                    "content": "You will be given a python script Please remove any dead code if it exists."
-                    },
-                    {
-                    "role": "user",
-                    "content": "display the optimised python script"
-                    }
-                    ]
-                }),
-            });
+  if (mytext) {
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+          },
+          body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content:
+                  "You will be given a python script Please remove any dead code if it exists.",
+              },
+              {
+                role: "user",
+                content: "display the optimised python script",
+              },
+            ],
+          }),
+        },
+      );
 
-            if (response.ok) {
-                const data = await response.json();
-                responseTextarea.value = data.choices[0].message.content;
-            } else {
-                responseTextarea.value = 'Error: Unable to process your request.';
-            }
-        } catch (error) {
-            console.error(error);
-            responseTextarea.value = 'Error: Unable to process your request.';
-        }
+      if (response.ok) {
+        const data = await response.json();
+        responseTextarea.value = data.choices[0].message.content;
+      } else {
+        responseTextarea.value = "Error: Unable to process your request.";
+      }
+    } catch (error) {
+      console.error(error);
+      responseTextarea.value = "Error: Unable to process your request.";
     }
+  }
 });
