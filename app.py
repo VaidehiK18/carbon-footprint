52bd4d6f4355e2d29219f4104ca7340b42188561
vaidehi <vaidehi@karmalife.ai>
2023-10-21 15:29:59 +0530

    Used json format to render output

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ def index():
 def execute_script():
     # Fetch the Python script content from the frontend
     filename = request.form['filename']
+    print(filename)
 
     # Save the script content to a temporary file
     with open('scripts/uploaded_script.py', 'w') as script_file:
         script_file.write(filename)
+        print(script_file)
 
     # Execute the script
     try:
@@ -25,12 +27,15 @@ def execute_script():
         output = e.output
 
     # The script generates a CSV file 'emissions.csv', read and render specific columns
-    csv_column = read_csv_column('emissions.csv', column_name=['emissions','emissions_rate','cpu_power','gpu_power','ram_power','cpu_energy','gpu_energy','ram_energy','energy_consumed'])
+    csv_column = pd.read_csv('emissions.csv')
+    column_name=['emissions','emissions_rate','cpu_energy','gpu_energy','ram_energy','energy_consumed']
+    csv_column = csv_column.tail(1)[column_name]
     print(f"csv_column = {csv_column}")
     # Remove the temporary script file
-    os.remove('scripts/uploaded_script.py')
-
-    return render_template('home.html', csv_column=csv_column)
+    os.remove('scripts/uploaded_script.py')  
+    csv_column = csv_column.to_json(orient='records')    
+    print(f"json_csv_column = {csv_column}")
+    return render_template("emissions.html", csv_column=csv_column)
 
 def read_csv_column(file_path, column_name):
     import csv
@@ -39,7 +44,6 @@ def read_csv_column(file_path, column_name):
         # Get the latest record, i.e the last one from emissions.csv
         data = df.tail(1)[column_name]
     return data
-
 
 if __name__ == '__main__':
     app.run(debug=True)
