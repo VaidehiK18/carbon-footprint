2700169a9f0ee2111a1f4fefc12905ba17c9b24b
Vidisha-18 <vidisha.k@esmagico.in>
2023-10-22 23:11:36 +0530

    Fixed binary file bug

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 from flask_cors import CORS
 import ast
+import sys
 
 
 app = Flask(__name__,static_url_path='/static')
@@ -16,39 +17,74 @@ def index():
 @app.route('/execute_script', methods=['POST'])
 def execute_script():
     # Fetch the Python script content from the frontend
+    if 'file' not in request.files:
+        return 'No file part', 400
+    
     uploaded_file = request.files['file']
-    print(uploaded_file)
-    uploaded_file.save(f'{os.getcwd()}/scripts/{uploaded_file.filename}')
+    if uploaded_file.filename == '':
+        return 'No selected file', 400
+    
+    # uploaded_file.save(f'{os.getcwd()}/scripts/{uploaded_file.filename}')
     
 
-    # Add headers
+    # # Add headers
+    # header = "from codecarbon import track_emissions"
+    # decorator = "@track_emissions()"
+    # function_start = "def track():"
+    # function_call = "track()"
+        
+    # with open('scripts/uploaded_script.py', 'w') as script_file:
+    #     script_file.write(uploaded_file.read())    
+
+    # # Check if the requirements file already exists, and if so, delete it
+    # requirements_file = 'requirements.txt'
+    # if os.path.exists(requirements_file):
+    #     os.remove(requirements_file)
+
+    # # Generate the requirements.txt file
+    # generate_requirements_file(f'{os.getcwd()}/scripts/uploaded_script.py', requirements_file)
+
+    # # Save the script content to a temporary file
+    # # with open('scripts/uploaded_script.py', 'r') as script_file:
+    # #     var = ast.parse(script_file.read())
+    # # modified_content = f"{header}\n{decorator}\n{function_start}\n\t{var}\n{function_call}"
+    # # print(modified_content)
+
+
+    # # Save the script content to a temporary file
+    # with open('scripts/uploaded_script.py', 'wb') as script_file:
+    #     script_file.write(uploaded_file.read())
+
+    # # Read the content from the temporary file as a string
+    # with open('scripts/uploaded_script.py', 'r', encoding='utf-8') as script_file:
+    #     script_content = script_file.read()
+
+    # # Modify the script content here if needed
+    # modified_content = f"{header}\n{decorator}\n{function_start}\n\t{script_content}\n{function_call}"
+
+    # # Write the modified content back to the temporary file
+    # with open('scripts/uploaded_script.py', 'w', encoding='utf-8') as script_file:
+    #     script_file.write(modified_content)
+    # Execute the script
+    with open('scripts/uploaded_script.py', 'wb') as script_file:
+        script_file.write(uploaded_file.read())
     header = "from codecarbon import track_emissions"
     decorator = "@track_emissions()"
     function_start = "def track():"
     function_call = "track()"
-        
-    with open('scripts/uploaded_script.py', 'w') as script_file:
-        script_file.write(uploaded_file.read())    
-
-    # Check if the requirements file already exists, and if so, delete it
-    requirements_file = 'requirements.txt'
-    if os.path.exists(requirements_file):
-        os.remove(requirements_file)
-
-    # Generate the requirements.txt file
-    generate_requirements_file(f'{os.getcwd()}/scripts/uploaded_script.py', requirements_file)
-
-    # Save the script content to a temporary file
-    with open('scripts/uploaded_script.py', 'r') as script_file:
-        var = ast.parse(script_file.read())
-    modified_content = f"{header}\n{decorator}\n{function_start}\n\t{var}\n{function_call}"
-    print(modified_content)
-    # Execute the script
+    with open('scripts/uploaded_script.py', 'rb') as script_file:
+        script_content_bytes = script_file.read()
+    
+    modified_content_bytes = b"{header}\n{decorator}\n{function_start}\n\t{script_content_bytes}\n{function_call}"
+    with open('scripts/uploaded_script.py', 'wb') as script_file:
+        script_file.write(modified_content_bytes)
     try:
         create_environment()
-        subprocess.check_output(['python', 'scripts/uploaded_script.py'], stderr=subprocess.STDOUT)
+        output = subprocess.check_output([sys.executable, 'scripts/uploaded_script.py'], stderr=subprocess.STDOUT, text=True)
+        print('Script Output:', output)
     except subprocess.CalledProcessError as e:
         output = e.output
+        print('Error Output:', output)
     
     # The script generates a CSV file 'emissions.csv', read and render specific columns
     csv_column = read_csv_column('emissions.csv', column_name=['emissions','emissions_rate','cpu_power','gpu_power','ram_power','cpu_energy','gpu_energy','ram_energy','energy_consumed'])
@@ -67,12 +103,12 @@ def execute_script():
     'ram_energy': float(csv_column['ram_energy'].iloc[0]),
     'energy_consumed': float(csv_column['energy_consumed'].iloc[0])
     }
-    
+    resp={}
     json_response = jsonify({"data":resp})
     myResponse = make_response(json_response)
     myResponse.status_code=200
         
-    # return render_template('home.html', csv_column=csv_column)
+    # # return render_template('home.html', csv_column=csv_column)
     return myResponse
 
 def create_environment():
--- scripts/uploaded_script.py
+++ scripts/uploaded_script.py
@@ -1,1 +1,5 @@
-C:\fakepath\quess_repeat_users.py
+{header}
+{decorator}
+{function_start}
+	{script_content_bytes}
+{function_call}
